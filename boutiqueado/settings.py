a9a7fe1b3746188f30a2e46fd48c95ad88fd8019
Kai <kmichaelmikhail@gmail.com>
2023-10-09 09:57:08 +0200

    Fix templates

--- boutiqueado/settings.py
+++ boutiqueado/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!@!o380mrk=7bp%56p14ha3%h!!%z6wl2o5^e)v2hd(=c)4f35'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv["DEBUG", '']
+DEBUG = True
 
 ALLOWED_HOSTS = ['boutique-walkthrough-69-8562d6bd4d60.herokuapp.com', '127.0.0.1']
 
